6565d6c037fa5d0fb0f3ac1c578ac374fb2302aa
Ganesh Vijayakumar <ganeshv@psu.edu>
2016-06-07 10:05:04 -0400

    Changing N_d to 3.0 and N_b to 2.0

--- Intestine/LBM.f90
+++ Intestine/LBM.f90
@@ -460,7 +460,7 @@ DO WHILE (ASSOCIATED(current))
       !------ Calculate effective radius: R_influence_P = R + (N_b *delta)
       !------ Note: need to convert this into Lattice units and not use the physical length units
       !------ Then compute equivalent cubic mesh length scale
-      N_b = 1.0
+      N_b = 2.0
       R_P  = current%pardata%rp
       Sh_P = current%pardata%sh
       delta_P = R_P / Sh_P
@@ -1119,7 +1119,7 @@ DO WHILE (ASSOCIATED(current))
 !------ Calculate effective radius: R_influence_P = R + (N_d *delta)
 !------ Note: need to convert this into Lattice units and not use the physical length units
 !------ Then compute equivalent cubic mesh length scale
-	N_d = 1.5
+	N_d = 3.0
         R_P  = current%pardata%rp
 	Sh_P = current%pardata%sh
         delta_P = R_P / Sh_P
