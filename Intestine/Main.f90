58d251e41116297066c3ff581236e74b1be7f2a6
Ganesh Vijayakumar <ganeshv@psu.edu>
2016-06-11 15:14:23 -0600

    Adding a call to PrintDrugConservation

--- Intestine/Main.f90
+++ Intestine/Main.f90
@@ -186,7 +186,7 @@ PROGRAM LBM3D	! 3D Parallelized LBM Simulation
         CALL PrintParticles						! output the particle velocity, radius, position and con. [MODULE: Output]
      ENDIF
 
-     CALL PrintScalar			! print the total absorbed/entering/leaving scalar as a function of time [MODULE: Output]
+     CALL PrintDrugConservation		! print the total absorbed/entering/leaving scalar as a function of time [MODULE: Output]
      ! CALL PrintMass			! print the total mass in the system (TEST)
      ! CALL PrintVolume			! print the volume in the system (TEST)
 
